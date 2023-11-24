6dec63caf4250acd78cbba0476aedc217a99ca05
ElenaVert <126357036+ElenaVert@users.noreply.github.com>
2023-11-24 19:17:41 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import telebot
+
+TOKEN = '6945475103:AAHZKXEHrIK0Tmj2Hzs-_RecDzckwraGi5w'
+bot = telebot.TeleBot(TOKEN)
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
